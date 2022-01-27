83a61dd76d427cf828670c84af9194a2ca9632b4
xTazzy <tmao23@student.dimanregional.org>
2022-01-27 11:42:05 -0500

    test
    funky test

---
+++ _01-Funky-TV/js/main.js
@@ -0,0 +1,8 @@
+/******************/
+/*  Feature Dev   */
+/******************/
+/* JS allows you to create an experience.
+/* Sound button
+/* Two buttons- two channels.  
+/* Change channel, load animated .gif
+/* We've been looking for you, Neo...     */
